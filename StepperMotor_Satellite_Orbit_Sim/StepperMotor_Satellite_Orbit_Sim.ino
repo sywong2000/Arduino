672372455d4b3da6bd9393ae10895663463a14b4
Stephen Wong <sywong2000@yahoo.com>
2024-07-04 00:25:09 +0800

    added uart configuration

--- StepperMotor_Satellite_Orbit_Sim/StepperMotor_Satellite_Orbit_Sim.ino
+++ StepperMotor_Satellite_Orbit_Sim/StepperMotor_Satellite_Orbit_Sim.ino
@@ -1,4 +1,6 @@
+#include <TMCStepper.h>         // TMCstepper - https://github.com/teemuatlut/TMCStepper
 #include <AccelStepper.h>
+#include <SoftwareSerial.h>     // Software serial for the UART to TMC2209 - https://www.arduino.cc/en/Reference/softwareSerial
 //#include <MultiStepper.h>
 
 int enablePin = 2;
@@ -16,27 +18,47 @@ long btnDelay = 40;
 long DbtnDelay = 400;
 long nPressed = 0;
 
-AccelStepper stepper(1, stepPin, dirPin);
+//AccelStepper stepper(1, stepPin, dirPin);
 int position = 0;
 int nStepSize = 40;
 int nSpeed = 800;
 
+
+#define SW_SCK           5      // Software Slave Clock (SCK) - BLUE
+#define SW_TX            6      // SoftwareSerial receive pin - BROWN
+#define SW_RX            7      // SoftwareSerial transmit pin - YELLOW
+#define DRIVER_ADDRESS   0b00   // TMC2209 Driver address according to MS1 and MS2
+#define R_SENSE 0.11f           // SilentStepStick series use 0.11 ...and so does my fysetc TMC2209 (?)
+
 // Create a new instance of the AccelStepper class:
-// AccelStepper stepper = AccelStepper(motorInterfaceType, stepPin, dirPin);
+SoftwareSerial SoftSerial(SW_RX, SW_TX);                          // Be sure to connect RX to TX and TX to RX between both devices
+TMC2209Stepper driver(&SoftSerial, R_SENSE, DRIVER_ADDRESS);   // Create TMC driver
+AccelStepper stepper = AccelStepper(stepper.DRIVER, stepPin, dirPin);
  
 void setup() {
   // Set the maximum speed in steps per second:
+  SoftSerial.begin(11520);           // initialize software serial for UART motor control
+  driver.beginSerial(11520);      // Initialize UART
+
+  driver.begin();                                                                                                                                                                                                                                                                                                                            // UART: Init SW UART (if selected) with default 115200 baudrate
+  driver.toff(5);                 // Enables driver in software
+  driver.rms_current(2500);        // Set motor RMS current
+  driver.microsteps(256);         // Set microsteps
+
+  driver.en_spreadCycle(false);
+  driver.pwm_autoscale(true);     // Needed for stealthChop
+
   pinMode(stepPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
   pinMode(enablePin, OUTPUT);
-  pinMode(ms1Pin, OUTPUT);
-  pinMode(ms2Pin, OUTPUT);
+  // pinMode(ms1Pin, OUTPUT);
+  // pinMode(ms2Pin, OUTPUT);
 
   stepper.setMaxSpeed(8000);
   stepper.setSpeed(1000);        
   stepper.setEnablePin(enablePin);
-  digitalWrite(ms1Pin, HIGH);
-  digitalWrite(ms2Pin, HIGH);
+  // digitalWrite(ms1Pin, HIGH);
+  // digitalWrite(ms2Pin, HIGH);
   stepper.disableOutputs();
 
   Serial.begin(115200);
@@ -54,7 +76,7 @@ void loop()
   stepper.setCurrentPosition(0);
  
   // Run the motor forward at 200 steps/second until the motor reaches 400 steps (2 revolutions):
-  nSpeed = 200;
+  nSpeed = 600;
   Serial.print("Running at speed:");
   Serial.println(nSpeed);
   t1 = millis();
