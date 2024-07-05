9b1eb7f2713fd98d2ebe72fcef399cfcf32df9fc
Stephen Wong <stephen.wong@cargosmart.com>
2024-07-05 17:49:32 +0800

    added logic with buttons triggers

--- StepperMotor_Satellite_Orbit_Sim/StepperMotor_Satellite_Orbit_Sim.ino
+++ StepperMotor_Satellite_Orbit_Sim/StepperMotor_Satellite_Orbit_Sim.ino
@@ -1,6 +1,6 @@
-#include <TMCStepper.h>         // TMCstepper - https://github.com/teemuatlut/TMCStepper
+#include <TMCStepper.h>  // TMCstepper - https://github.com/teemuatlut/TMCStepper
 #include <AccelStepper.h>
-#include <SoftwareSerial.h>     // Software serial for the UART to TMC2209 - https://www.arduino.cc/en/Reference/softwareSerial
+#include <SoftwareSerial.h>  // Software serial for the UART to TMC2209 - https://www.arduino.cc/en/Reference/softwareSerial
 //#include <MultiStepper.h>
 
 int enablePin = 2;
@@ -10,10 +10,10 @@ int dirPin = 3;
 int ms1Pin = 8;
 int ms2Pin = 9;
 
-int btnA = 10;
-int btnB = 9;
-int btnC = 8;
-int btnD = 11;
+int btn11 = 11;
+int btn12 = 12;
+int btn13 = 13;
+int btn14 = 14;
 long btnDelay = 40;
 long DbtnDelay = 400;
 long nPressed = 0;
@@ -26,29 +26,29 @@ int nStepSize = 40;
 int nSpeed = 800;
 int brightness = 0;
 
-#define SW_SCK           5      // Software Slave Clock (SCK) - BLUE
-#define SW_TX            6      // SoftwareSerial receive pin - BROWN
-#define SW_RX            7      // SoftwareSerial transmit pin - YELLOW
-#define DRIVER_ADDRESS   0b00   // TMC2209 Driver address according to MS1 and MS2
-#define R_SENSE 0.11f           // SilentStepStick series use 0.11 ...and so does my fysetc TMC2209 (?)
+#define SW_SCK 5             // Software Slave Clock (SCK) - BLUE
+#define SW_TX 6              // SoftwareSerial receive pin - BROWN
+#define SW_RX 7              // SoftwareSerial transmit pin - YELLOW
+#define DRIVER_ADDRESS 0b00  // TMC2209 Driver address according to MS1 and MS2
+#define R_SENSE 0.11f        // SilentStepStick series use 0.11 ...and so does my fysetc TMC2209 (?)
 
 // Create a new instance of the AccelStepper class:
-SoftwareSerial SoftSerial(SW_RX, SW_TX);                          // Be sure to connect RX to TX and TX to RX between both devices
-TMC2209Stepper driver(&SoftSerial, R_SENSE, DRIVER_ADDRESS);   // Create TMC driver
+SoftwareSerial SoftSerial(SW_RX, SW_TX);                      // Be sure to connect RX to TX and TX to RX between both devices
+TMC2209Stepper driver(&SoftSerial, R_SENSE, DRIVER_ADDRESS);  // Create TMC driver
 AccelStepper stepper = AccelStepper(stepper.DRIVER, stepPin, dirPin);
- 
+
 void setup() {
   // Set the maximum speed in steps per second:
-  SoftSerial.begin(11520);           // initialize software serial for UART motor control
-  driver.beginSerial(11520);      // Initialize UART
+  SoftSerial.begin(11520);    // initialize software serial for UART motor control
+  driver.beginSerial(11520);  // Initialize UART
 
-  driver.begin();                                                                                                                                                                                                                                                                                                                            // UART: Init SW UART (if selected) with default 115200 baudrate
-  driver.toff(5);                 // Enables driver in software
-  driver.rms_current(2500);        // Set motor RMS current
-  driver.microsteps(256);         // Set microsteps
+  driver.begin();            // UART: Init SW UART (if selected) with default 115200 baudrate
+  driver.toff(5);            // Enables driver in software
+  driver.rms_current(2500);  // Set motor RMS current
+  driver.microsteps(256);    // Set microsteps
 
   driver.en_spreadCycle(false);
-  driver.pwm_autoscale(true);     // Needed for stealthChop
+  driver.pwm_autoscale(true);  // Needed for stealthChop
 
   pinMode(stepPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
@@ -57,79 +57,56 @@ void setup() {
   // pinMode(ms2Pin, OUTPUT);
 
   stepper.setMaxSpeed(8000);
-  stepper.setSpeed(1000);        
+  stepper.setSpeed(1000);
   stepper.setEnablePin(enablePin);
   // digitalWrite(ms1Pin, HIGH);
   // digitalWrite(ms2Pin, HIGH);
   stepper.disableOutputs();
 
   Serial.begin(115200);
-  
+
   //default brightness
   brightness = 128;
-  brightness = map(brightness, 0,255,0,255);
-
+  brightness = map(brightness, 0, 255, 0, 255);
 }
 
 // 100 steps half circle full step
 // 1600 steps half circle 1/16
 long int t1, t2, d;
+bool bSimStarted = false;
 
- 
-void loop() 
-{ 
-  // Set the current position to 0:
-  stepper.setCurrentPosition(0);
- 
-  nSpeed = 600;
-  Serial.print("Running at speed:");
-  Serial.println(nSpeed);
-  t1 = millis();
 
-  while(stepper.currentPosition() < 1600)
-  {
-    stepper.setSpeed(nSpeed);
-    stepper.runSpeed();
-  }
-  d = (millis() - t1);
-  Serial.print("Duration: ");
-  Serial.print(d);
-  Serial.println(" ms.");
- 
-  delay(1000);
- 
-  nSpeed = -6.6;
-  Serial.print("Running at speed:");
-  Serial.println(nSpeed);
-  t1 = millis();
+void loop() {
 
-  while(stepper.currentPosition() >=0) 
-  {
+  nSpeed = 1200;
+  while (digitalRead(btn11) == LOW) {
+    bSimStarted = false;
     stepper.setSpeed(nSpeed);
     stepper.runSpeed();
   }
 
+  // d = (millis() - t1);
+  // Serial.print("Duration: ");
+  // Serial.print(d);
+  // Serial.println(" ms.");
 
-  d = millis() - t1;
-  Serial.print("Duration: ");
-  Serial.print(d);
-  Serial.println(" ms.");
-
-  
-
-  analogWrite(ledPin, brightness);
- 
   // delay(1000);
- 
-  // // Reset the position to 0:
-  // stepper.setCurrentPosition(0);
- 
-  // // Run the motor forward at 400 steps/second until the motor reaches 600 steps (3 revolutions):
-  // while(stepper.currentPosition() != 600)
-  // {
-  //   stepper.setSpeed(400);
-  //   stepper.runSpeed();
-  // }
- 
-  // delay(3000);
+
+  if (digitalRead(btn12) == LOW && !bSimStarted) {
+    bSimStarted = true;
+    t1 = millis();
+    nSpeed = -6.6;
+    Serial.print("Running at speed:");
+    Serial.println(nSpeed);
+    t1 = millis();
+
+    // 150 degress / 1.8 degree per step * 256 microsteps = 21333
+
+    stepper.setCurrentPosition(0);
+  }
+
+  while (bSimStarted && stepper.currentPosition() < 21300) {
+    stepper.setSpeed(nSpeed);
+    stepper.runSpeed();
+  }
 }
