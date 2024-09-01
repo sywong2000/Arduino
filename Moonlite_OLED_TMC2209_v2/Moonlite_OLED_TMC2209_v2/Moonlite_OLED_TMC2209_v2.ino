8297028bb1a2d15176062e9d340bd49a672714fc
Stephen Wong <sywong2000@yahoo.com>
2024-09-01 14:00:20 +0800

    basic version of moonlite based on TMC2209 driver, support speed and half step control

---
+++ Moonlite_OLED_TMC2209_v2/Moonlite_OLED_TMC2209_v2/Moonlite_OLED_TMC2209_v2.ino
@@ -0,0 +1,337 @@
+#include <EEPROM.h>
+#include <TMCStepper.h>         // TMCstepper - https://github.com/teemuatlut/TMCStepper
+#include <AccelStepper.h>
+#include <SoftwareSerial.h>     // Software serial for the UART to TMC2209 - https://www.arduino.cc/en/Reference/softwareSerial
+
+
+#define SW_SCK           5      // Software Slave Clock (SCK) - BLUE
+#define SW_TX            6      // SoftwareSerial receive pin - BROWN
+#define SW_RX            7      // SoftwareSerial transmit pin - YELLOW
+#define DRIVER_ADDRESS   0b00   // TMC2209 Driver address according to MS1 and MS2
+#define R_SENSE 0.11f           // SilentStepStick series use 0.11 ...and so does my fysetc TMC2209 (?)
+
+const int enablePin = 2;
+const int stepPin = 4;
+const int dirPin = 3;
+const int ms1Pin = 8;
+const int ms2Pin = 9;
+
+SoftwareSerial SoftSerial(SW_RX, SW_TX);                          // Be sure to connect RX to TX and TX to RX between both devices
+TMC2209Stepper driver(&SoftSerial, R_SENSE, DRIVER_ADDRESS);   // Create TMC driver
+AccelStepper stepper = AccelStepper(stepper.DRIVER, stepPin, dirPin);
+
+unsigned long currentPosition = 10000;
+unsigned long targetPosition = 10000;
+unsigned long lastSavedPosition = 0;
+
+bool eoc = false;
+bool bHalfStep = false;
+int LineLen= 0;
+String cmd, param, line;
+const long nSpeedConstant = 50;
+long nSpeedFactor = 10;
+long nSpeed = nSpeedConstant* nSpeedFactor;
+
+float tCoeff = 0;
+bool bSetToMove = false;
+
+
+long hexstr2long(String hexstr)
+{
+  char buf[hexstr.length() + 1];
+  hexstr.toCharArray(buf, hexstr.length() + 1);
+  return strtol(buf, NULL, 16);
+}
+
+
+void setup() {
+
+  SoftSerial.begin(115200);           // initialize software serial for UART motor control
+  driver.beginSerial(115200);      // Initialize UART
+  driver.begin();                                                                                                                                                                                                                                                                                                                            // UART: Init SW UART (if selected) with default 115200 baudrate
+  driver.toff(5);                 // Enables driver in software
+  driver.rms_current(800);        // Set motor RMS current
+  driver.microsteps(16);         // Set microsteps
+  driver.en_spreadCycle(false);
+  driver.pwm_autoscale(true);     // Needed for stealthChop
+
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+  pinMode(enablePin, OUTPUT);
+
+  stepper.setEnablePin(enablePin);
+  stepper.disableOutputs();
+  stepper.setMaxSpeed(nSpeed);
+  stepper.setSpeed(nSpeed);
+
+  // init position from EEPROM
+  // default position is 10000
+  EEPROM.get(0, currentPosition);
+  if (isnan(currentPosition) || currentPosition<0 || currentPosition>65535)
+  {
+    EEPROM.put(0, (long)10000);
+  }
+  stepper.setCurrentPosition(currentPosition);
+  lastSavedPosition = currentPosition;
+  targetPosition = currentPosition;
+
+  Serial.begin(9600);
+}
+
+
+void loop() {
+
+  // process when end of command
+  if (eoc)
+  {
+    if (line.startsWith("2"))
+    {
+      //debugSerial.println("Got Dual focuser command(?) starting with 2. Send values of first motor");
+      // remove first character and parse command
+      line = line.substring(1);
+    }
+
+    LineLen = line.length();
+    if (LineLen >= 2)
+    {
+      cmd = line.substring(0, 2);
+    }
+    else
+    {
+      cmd = line.substring(0, 1);
+    }
+
+    if (LineLen > 2)
+    {
+      param = line.substring(2);
+    }
+
+    cmd.trim();
+    param.trim();
+
+
+    // GB - get back light value
+    if (cmd.equalsIgnoreCase("GB"))
+    {
+      Serial.print("00#");
+    }
+
+    // PH - Find Motor Home
+    if (cmd.equalsIgnoreCase("PH"))
+    {
+      stepper.setCurrentPosition(10000);
+      stepper.moveTo(0);
+    }
+
+    // GV - firmware version
+    if (cmd.equalsIgnoreCase("GV"))
+    {
+      Serial.print("10#");
+    }
+
+    // C - temperature conversion
+    if (cmd.equalsIgnoreCase("C"))
+    {
+      // Serial.print("10#");
+    }
+
+    // GP - get current position
+    if (cmd.equalsIgnoreCase("GP"))
+    {
+      currentPosition = stepper.currentPosition();
+      char tempString[6];
+      sprintf(tempString, "%04lX", currentPosition);
+      Serial.print(tempString);
+      Serial.print("#");
+    }
+
+    // GN - Get New Position
+    if (cmd.equalsIgnoreCase("GN"))
+    {
+      targetPosition = stepper.targetPosition();
+      char tempString[6];
+      sprintf(tempString, "%04lX", targetPosition);
+      Serial.print(tempString);
+      Serial.print("#");
+    }
+
+    // GT - get temperature
+    if (cmd.equalsIgnoreCase("GT"))
+    {
+    //      sensors.requestTemperatures();
+      float temperature = 25.0;//sensors.getTempCByIndex(0);
+      if (temperature > 100 || temperature < -50)
+      {
+        // error
+        temperature = 0;
+      }
+      byte t_int = (byte)temperature << 1;
+      t_int += round(temperature - (byte)temperature);
+      Serial.print(t_int, HEX);
+      Serial.print('#');
+    }
+
+    // GC - Get temperature coefficient
+    if (cmd.equalsIgnoreCase("GC"))
+    {
+      // Serial.print("02#");
+      Serial.print((byte)tCoeff, HEX);
+      Serial.print('#');
+    }
+
+    // SC - Set temperature coefficient
+    if (cmd.equalsIgnoreCase("SC"))
+    {
+      if (param.length() > 4)
+      {
+        param = param.substring(param.length() - 4);
+      }
+      if (param.startsWith("F"))
+      {
+        tCoeff = ((0xFFFF - strtol(param.c_str(), NULL, 16)) / -2.0f) - 0.5f;
+      }
+      else
+      {
+        tCoeff = strtol(param.c_str(), NULL, 16) / 2.0f;
+      }
+    }
+
+    // GD - Get Motor Speed
+    // 20 - 15pps
+    // 10 - 30pps
+    // 08 - 60pps
+    // 04 - 125pps
+    // 02 - 250pps
+
+    if (cmd.equalsIgnoreCase("GD"))
+    {
+      char tempString[6];
+      sprintf(tempString, "%02X", nSpeedFactor*32);
+      Serial.print(tempString);
+      Serial.print("#");
+    }
+
+    // SD - Set Motor Speed
+    // 20 - 15pps
+    // 10 - 30pps
+    // 08 - 60pps
+    // 04 - 125pps
+    // 02 - 250pps
+
+    if (cmd.equalsIgnoreCase("SD"))
+    {
+      nSpeedFactor = 32/hexstr2long(param);
+      // SpeedFactor: smaller value means faster
+      nSpeed = nSpeedConstant * nSpeedFactor;
+      stepper.setMaxSpeed(nSpeed);
+      stepper.setSpeed(nSpeed);
+    }
+
+
+    // get half step    
+    if (cmd.equalsIgnoreCase("GH"))
+    {
+      if (bHalfStep)
+      {
+        Serial.print("FF#");
+      }
+      else
+      {
+        Serial.print("00#");
+      }
+    }
+
+    // full step
+    if (cmd.equalsIgnoreCase("SF"))
+    {
+      bHalfStep = false;
+      driver.microsteps(8);         // Set microsteps
+    }
+
+    // half step
+    if (cmd.equalsIgnoreCase("SH"))
+    {
+      bHalfStep = true;
+      driver.microsteps(16);
+    }
+
+    // GI - is Motor Moving
+    if (cmd.equalsIgnoreCase("GI"))
+    {
+      if (stepper.distanceToGo() != 0 && bSetToMove)
+      {
+        Serial.print("01#");
+      }
+      else
+      {
+        Serial.print("00#");
+      }
+    }
+    // set current motor position
+    if (cmd.equalsIgnoreCase("SP"))
+    {
+      currentPosition = hexstr2long(param);
+      stepper.setCurrentPosition(currentPosition);
+    }
+
+    // set new motor position
+    if (cmd.equalsIgnoreCase("SN"))
+    {
+      targetPosition = hexstr2long(param);
+      stepper.moveTo(targetPosition);
+    }
+
+    // initiate a move
+    if (cmd.equalsIgnoreCase("FG"))
+    {
+      bSetToMove = true;
+    }
+
+    // stop a move
+    if (cmd.equalsIgnoreCase("FQ"))
+    {
+      bSetToMove = false;
+      stepper.stop();
+    
+    }
+    line = "";
+    eoc = false;
+  }  // eoc == true
+
+  if (stepper.distanceToGo()!=0 && bSetToMove)
+  {
+    stepper.setSpeed( targetPosition< stepper.currentPosition()?nSpeed*-1:nSpeed);
+    stepper.runSpeed();
+  }
+
+}
+
+
+// For sparkfun pro micro
+void serialEventRun(void) {
+  if (Serial.available()) serialEvent();
+}
+
+void serialEvent() {
+  while (Serial.available()>0 && !eoc)
+  {
+    char c = Serial.read();
+    if (c != '#' && c != ':')
+    {
+      line = line + c;
+    }
+    else
+    {
+      if (c == '#')
+      {
+        eoc = true;
+        // break and handle the full command first
+        break;
+      }
+      if (c == ':')
+      {
+        line = "";
+      }
+    }
+  }
+}
